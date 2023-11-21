e98ea7eb50b0e1e6e3c7e58b22122b8bfc5955be
Tapan Patel <tapan2930@gmail.com>
2023-11-21 00:28:11 -0500

    Tapan@added payment configuration for stripe and extend templates

---
+++ Payments/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.payment_view, name='Payments'),
+    path('/success', views.payment_success_view, name='Payments_Success'),
+]
