ca12f66b4db03fdc8e65fe9b7dedf5f1f3211924
Dhruvesh Panchal <78642916+Dhruvesh8866@users.noreply.github.com>
2023-11-25 19:47:49 -0500

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,26 +1,24 @@
-from django.urls import path, include, path
-from django.conf import settings
-from django.conf.urls.static import static
-from . import views
-# from CryptoCrackers import views
-from .views import signup, user_login, index, user_profile, logout_view
+"""
+URL configuration for CryptoWebsite project.
 
-
-app_name = 'CoinCraft'
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('login/', views.user_login, name='login'),
-    path('signup/', views.signup, name='signup'),
-    path('portfolio/', views.portfolio, name='portfolio'),
-    path('profile/', user_profile, name='user_profile'),
-    path('logout/', logout_view, name='logout'),
+    path('admin/', admin.site.urls),
+    path('crypto/', include('Crypto.urls', namespace='CryptoCrackers'))
 
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
