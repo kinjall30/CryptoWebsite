b9114f7fcc561c2101cecb47ade1bc6705291a8a
Dhruvesh Panchal <78642916+Dhruvesh8866@users.noreply.github.com>
2023-11-25 19:46:47 -0500

    Add files via upload

---
+++ forms.py
@@ -0,0 +1,21 @@
+# forms.py
+from django import forms
+from .models import UserDetails
+
+
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = UserDetails
+        fields = ['username', 'email', 'password', 'first_name', 'last_name', 'date_of_birth']
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+# my added code below
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserDetails
+        fields = ['username', 'email', 'password', 'first_name', 'last_name', 'date_of_birth']
---
+++ urls.py
@@ -0,0 +1,26 @@
+from django.urls import path, include, path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+# from CryptoCrackers import views
+from .views import signup, user_login, index, user_profile, logout_view
+
+
+app_name = 'CoinCraft'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login/', views.user_login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('profile/', user_profile, name='user_profile'),
+    path('logout/', logout_view, name='logout'),
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
